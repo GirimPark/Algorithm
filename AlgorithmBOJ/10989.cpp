1c0eec3636a06808b8acca8a9e502462ddbce1a5
girm1999 <zzrlfla@naver.com>
2021-12-14 02:54:45 +0900

    ect 2

---
+++ AlgorithmBOJ/10989.cpp
@@ -0,0 +1,22 @@
+//#include <iostream>
+//#include <vector>
+//using namespace std;
+//
+//#define MAX 10001
+//
+//int main() {
+//	int n, num;
+//	scanf("%d", &n);
+//	vector<int> count(MAX);
+//	for (int i = 0; i < n; i++) {
+//		scanf("%d", &num);
+//		count[num]++;
+//	}
+//	
+//	for (int i = 1; i < count.size(); i++) {
+//		for (int j = 0; j < count[i]; j++)
+//			printf("%d\n", i);
+//	}
+//		
+//	return 0;
+//}
---
+++ AlgorithmBOJ/11652.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main() {
+	int n;
+	scanf("%d", &n);
+	vector<long long> card(n);
+	for (int i = 0; i < n; i++) 
+		scanf("%lld", &card[i]);
+	
+	sort(card.begin(), card.end());
+
+	int max = 1, cnt = 1;
+	long long answer = card[0];
+	for (int i = 1; i < n; i++) {
+		if (card[i] == card[i - 1]) {
+			cnt++;
+			if (cnt > max) {
+				max = cnt;
+				answer = card[i];
+			}
+		}
+		else 
+			cnt = 1;
+		
+	}
+
+	printf("%lld", answer);
+
+	return 0;
+}
